66c383a6095e855cf6abc83107240ffa7e5cba7a
Mike Wittie <mwittie@cs.montana.edu>
2020-02-09 13:13:03 -0700

    added command line parsing examples

---
+++ args_and_config_files/main.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <unistd.h>
+
+//#define BASIC
+#define OPTIONS
+//#define CONFIG_FILES
+
+int main(int argc, char *argv[]) {
+   printf("Hello World!\n");
+
+#ifdef BASIC
+   // read command line arguments as strings
+   for (int i = 0; i < argc; ++i)
+      printf("Argument %d is %s\n", i, argv[i]);
+
+   /**
+    * Exercise: parse "main -n1 filename" as in "head -n1 filename"
+    */
+
+   /*
+    * Conclusions:
+    * - Whatever you do, don't write your own parser!
+    */
+#endif
+
+#ifdef OPTIONS
+   // parse command line arguments as options (named arguments)
+   int opt;
+   int aflag = 0;
+   int bflag = 0;
+   char *cvalue = NULL;
+   // ":" follows an option name to get argument from optarg
+   while((opt = getopt(argc, argv, "abc:")) != -1){
+      switch(opt){
+         case 'a':
+            printf("option: %c\n", opt);
+            aflag = 1;
+            break;
+         case 'b':
+            printf("option: %c\n", opt);
+            bflag = 1;
+            break;
+         case 'c':
+            printf("option %c, value: %s\n", opt, optarg);
+            cvalue = optarg;
+            break;
+         case '?':
+            printf("unknown option: %c\n", optopt);
+            break;
+      }
+   }
+
+   printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+           aflag, bflag, cvalue);
+
+   // optind is for the extra arguments that are not parsed
+   for(; optind < argc; optind++){
+      printf("extra argument: %s\n", argv[optind]);
+   }
+
+   /**
+    * Conclusions:
+    * + good for calling multiple commands with scripts
+    * - cumbersome with many options
+    */
+#endif
+
+#ifdef CONFIG_FILES
+   // parse options from a configuration file
+
+
+   /**
+    * Conclusions:
+    * + many configuration file options INI, YAML, TOML, JSON, https://www.hyperrealm.com/libconfig/libconfig.html, http://www.config4star.org/
+    * - readability vs features vs correctness: https://github.com/crdoconnor/strictyaml
+    */
+#endif
+
+   return 0;
+}
