3bd690a1cdff3c25a0d0d7080e9b7a3f37817b39
Mike Wittie <mwittie@cs.montana.edu>
2020-02-21 07:50:59 -0700

    added a custom exception

--- tdd/test/tests.cpp
+++ tdd/test/tests.cpp
@@ -3,15 +3,26 @@
 
 #include "Rectangle.h"
 
+/**
+ * Discussion:
+ * - EXPECT vs ASSERT
+ * - string comparison: ASSERT_STREQ
+ * - floating point comparison: ASSERT_DOUBLE_EQ
+ * - testing with objects
+ * - using fixtures
+ * - FAIL() (for example in switch default)
+ * - testing with exception
+ */
+
 TEST(BasicTest, Success){
    ASSERT_EQ(1,1);
 }
 
 
-//TEST(RectangleInitializeBasic, Initialize_Correct){
-//   Rectangle r;
-//   EXPECT_NO_FATAL_FAILURE(r.initialize(0, 0, 1, 1));
-//}
+TEST(RectangleInitializeBasic, Initialize_Correct){
+   Rectangle r;
+   EXPECT_NO_FATAL_FAILURE(r.initialize(0, 0, 1, 1));
+}
 
 
 //class RectangleInitialize: public ::testing::Test{
