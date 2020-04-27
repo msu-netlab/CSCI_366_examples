14a19a46941e52b48e3f9371671095dc692ec547
Mike Wittie <mwittie@cs.montana.edu>
2020-04-27 11:28:02 -0600

    add shared variables example

--- threads/main.cpp
+++ threads/main.cpp
@@ -109,11 +109,52 @@ void run_example4(int color, sem_t *sem){
    printf("\033[%dmThread %" PRIu64 " exiting\033[0m\n", color, id);
 }
 
+/**
+ * Examle 5: A thread function using shared variables
+ * @param sleep_s
+ */
+int global_var = 0; // create a global variable for sharing between threads - needs to be declared above its use in code
+void run_example5(int color, sem_t *sem){
+   int sleep_s = 1;
+
+   static int static_var = 0; // create a static variable for sharing between threads
+   int local_var = 0; // create a local variable for this thread
+
+   // get thread id into a string and convert to an int
+   std::stringstream ss;
+   ss << std::this_thread::get_id();
+   uint64_t id = std::stoull(ss.str());
+
+   printf("\033[%dmThread %" PRIu64 " starting\033[0m\n", color, id);
+
+   // acquire the semaphore
+   sem_wait(sem); // will proceed if can decrement the semaphore
+
+   for(int i=0; i<2; i++) {
+      // increment variables and print out values
+      global_var++;
+      static_var++;
+      local_var++;
+      printf("\033[%dmThread %" PRIu64 " global_var=%d, static_var=%d, local_var=%d \033[0m\n", color, id, global_var, static_var, local_var);
+
+      // sleep
+      printf("\033[%dmThread %" PRIu64 " sleeping for %d seconds\033[0m\n", color, id, sleep_s);
+      std::this_thread::sleep_for(std::chrono::seconds(sleep_s));
+   }
+
+   // signal the semaphore
+   sem_post(sem); // increment the semaphore for the other thread
+
+   printf("\033[%dmThread %" PRIu64 " exiting\033[0m\n", color, id);
+}
+
+
 
 int main() {
 
    thread threads[2]; //set up a pool of threads, so we can join them before exiting
    sem_t sem;         //set up a semaphore
+
 
    sem_init(&sem, 0, 1);
 
@@ -132,10 +173,15 @@ int main() {
 //   threads[0] = thread(run_example3, 31);
 //   threads[1] = thread(run_example3, 32);
 
-//   // Example 4: start the threads semaphore blocks
+//   // Example 4: start the threads with semaphore blocks
 //   printf("Starting threads\n");
 //   threads[0] = thread(run_example4, 31, &sem);
 //   threads[1] = thread(run_example4, 32, &sem);
+
+//   // Example 5: start the threads with shared variables
+//   printf("Starting threads\n");
+//   threads[0] = thread(run_example5, 31, &sem);
+//   threads[1] = thread(run_example5, 32, &sem);
 
    // wait for the threads to finish
    for(int i=0; i<2; i++)
