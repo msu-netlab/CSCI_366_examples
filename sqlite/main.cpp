c3dc770cd634de4824fec4f1cd3a2a06d1fc8392
Mike Wittie <mwittie@cs.montana.edu>
2020-05-01 15:56:57 -0600

    finished sqlite example

--- sqlite/main.cpp
+++ sqlite/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <sqlite_modern_cpp.h>
 
-using namespace  sqlite;
+using namespace sqlite;
 using namespace std;
 
 int main() {
 
+   /**
+    * See https://github.com/SqliteModernCpp/sqlite_modern_cpp for more examples
+    */
+
    try{
       database db("company.sqlite");
 
-      db << "select Fname from EMPLOYEE;"
-         >> [&](string fname) {
-            cout << fname << endl;
+      db << "select Bdate, Address from EMPLOYEE where Fname = ? and Lname = ?;"
+         << "John"
+         << "Smith"
+         >> [&](string Bdate, string Address) {
+            cout << Bdate << ' ' << Address << endl;
          };
    }
    catch (exception& e) {
